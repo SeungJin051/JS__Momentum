bf329b1f848a5256b4a1e0ad39f7d735361717cf
SeungJin051 <83889135+SeungJin051@users.noreply.github.com>
2021-06-10 23:41:14 +0900

    #2

--- weather.js
+++ weather.js
@@ -33,7 +33,7 @@ function handleGeoSucces(position) {
 }
 
 function handleGeoError() {
-  console.log("Cant access geo location");
+  console.log("Can't access geo location");
 }
 
 function askForCoords() {
